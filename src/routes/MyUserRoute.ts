6bdf3176c3d0ddef1d1e3b8e9770633f26a00b55
Mr-Vitalii <co4ckowoi.v@gmail.com>
2024-03-12 18:47:36 +0200

    added the ability to update user data

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import ctrl from "../controllers/MyUserController.ts";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 const router = express.Router();
 
 //* /api/my/user
 router.post("/", jwtCheck, ctrl.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  ctrl.updateCurrentUser
+);
 
 export default router;
